09b56c62f36bcafb983fabaacd10e73af38a194a
fyirass <firas.mansour@esprit.tn>
2022-07-07 01:34:17 +0100

    dark mode commit

--- Maths_survey_app/wsgi.py
+++ Maths_survey_app/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Maths_survey_app.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Maths_survey_app.settings'
+
 
 application = get_wsgi_application()
