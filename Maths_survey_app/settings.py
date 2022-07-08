df10439a848fbe53d4146c02366bfd0dc0f18a1e
fyirass <firas.mansour@esprit.tn>
2022-07-08 02:26:44 +0100

    Final version

--- Maths_survey_app/settings.py
+++ Maths_survey_app/settings.py
@@ -114,6 +114,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.0/howto/static-files/
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Maths_survey_app/static')]
 
 
 # Extra places for collectstatic to find static files.
@@ -126,18 +133,11 @@ USE_TZ = True
 #### sendig email setting
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'mathsurveyapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'jtyyaeyziminnlig'
+EMAIL_HOST_USER = 'youandmathfeedback@gmail.com'
+EMAIL_HOST_PASSWORD = 'uodfkyxjocugcais'
 EMAIL_USE_TLS = True
 
 
 
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'Maths_survey_app/static')]
